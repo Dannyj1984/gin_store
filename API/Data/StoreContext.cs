9fdde78080624db9a89aea942448c098c3365d1b
Danny Jebb <dannyjebb@gmail.com>
2022-08-23 16:55:26 +0100

    update

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext //Derive from DBContext class
+    {
+        public StoreContext(DbContextOptions options) : base(options) //DbContext is base class
+        {
+
+        }
+
+        //products will be the name of the sql table
+        public DbSet<Product> Products {get; set;}
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+using System;
+using API.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace API
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            //'using' is the equivelant of adding finally to a try/catch block
+            //disposes of any resources scope is using
+            using var scope = host.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+            //Log any exceptions inside the program class and show on terminal
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            try
+            {
+                context.Database.Migrate(); // Does the same as dotnet ef database update on CLI
+                DbInitilizer.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Problem migrating data");
+            }
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
