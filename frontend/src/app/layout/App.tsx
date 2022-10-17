cb12c7ce5985133a25ff618e6d74fc82c32f9ae3
Danny Jebb <dannyjebb@gmail.com>
2022-10-17 15:27:33 +0100

    names added to account

--- frontend/src/app/layout/App.tsx
+++ frontend/src/app/layout/App.tsx
@@ -24,11 +24,7 @@ import { fetchCurrentUser } from "../../features/account/accountSlice";
 import PrivateRoute from "./PrivateRoute";
 import Orders from "../../features/orders/Orders";
 import Inventory from "../../features/admin/Inventory";
-import InventorySkeleton from "../../features/admin/InventorySkeleton";
-<<<<<<< HEAD
 import Profile from "../../features/account/Profile";
-=======
->>>>>>> 6951bc64db191d4a22f340a7e65781d76d752de1
 
 function App() {
   const dispatch = useAppDispatch();
@@ -91,10 +87,7 @@ function App() {
           <Route path='/basket' component={BasketPage} />
           <PrivateRoute path='/checkout' component={CheckoutPage} />
           <PrivateRoute path='/orders' component={Orders} />
-<<<<<<< HEAD
           <PrivateRoute path='/profile' component={Profile} />
-=======
->>>>>>> 6951bc64db191d4a22f340a7e65781d76d752de1
           <PrivateRoute roles={['Admin']} path='/inventory' component={Inventory} />
           <Route path='/login' component={Login} />
           <Route path='/register' component={Register} />
